7529a386b0fb6843524cbc814869075949caa4b4
MunrraMT <dev.armsantos@gmail.com>
2023-09-13 10:29:01 -0400

    Creating Slugs

--- rumbl/lib/rumbl/multimedia/video.ex
+++ rumbl/lib/rumbl/multimedia/video.ex
@@ -9,6 +9,7 @@ defmodule Rumbl.Multimedia.Video do
     field(:description, :string)
     field(:title, :string)
     field(:url, :string)
+    field(:slug, :string)
 
     belongs_to(:user, User)
     belongs_to(:category, Category)
@@ -22,5 +23,22 @@ defmodule Rumbl.Multimedia.Video do
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
     |> assoc_constraint(:category)
+    |> slugify_title()
+  end
+
+  defp slugify_title(changeset) do
+    case fetch_change(changeset, :title) do
+      {:ok, new_title} ->
+        put_change(changeset, :slug, slugify(new_title))
+
+      :error ->
+        changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
