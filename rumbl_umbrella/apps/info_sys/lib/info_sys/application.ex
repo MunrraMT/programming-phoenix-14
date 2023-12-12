17731f8ef10ad78a870ed6d12bb0a4a041821ca5
MunrraMT <dev.armsantos@gmail.com>
2023-12-11 22:29:49 -0400

    Adding Failover

--- rumbl_umbrella/apps/info_sys/lib/info_sys/application.ex
+++ rumbl_umbrella/apps/info_sys/lib/info_sys/application.ex
@@ -10,6 +10,8 @@ defmodule InfoSys.Application do
     children = [
       # Starts a worker by calling: InfoSys.Worker.start_link(arg)
       # {InfoSys.Worker, arg}
+
+      {InfoSys.Counter, 10}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- rumbl_umbrella/apps/info_sys/lib/info_sys/counter.ex
+++ rumbl_umbrella/apps/info_sys/lib/info_sys/counter.ex
@@ -7,14 +7,26 @@ defmodule InfoSys.Counter do
   def decrement(pid), do: GenServer.cast(pid, :decrement)
   def value(pid), do: GenServer.call(pid, :value)
 
-  def start_link(initial_value \\ 0) do
+  def start_link(initial_value) do
     GenServer.start_link(__MODULE__, initial_value, name: __MODULE__)
   end
 
   # server side
 
-  def init(initial_value), do: {:ok, initial_value}
+  def init(initial_value) do
+    Process.send_after(self(), :tick, 1000)
+    {:ok, initial_value}
+  end
+
   def handle_cast(:increment, value), do: {:noreply, value + 1}
   def handle_cast(:decrement, value), do: {:noreply, value - 1}
   def handle_call(:value, _from, value), do: {:reply, value, value}
+
+  def handle_info(:tick, 0), do: raise("Boom!")
+
+  def handle_info(:tick, value) do
+    IO.puts("tick #{value}")
+    Process.send_after(self(), :tick, 1000)
+    {:noreply, value - 1}
+  end
 end
